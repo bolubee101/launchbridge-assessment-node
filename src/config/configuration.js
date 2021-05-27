fd698d3e7419d7b448afae525d33b2bf1c16385a
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2021-05-27 12:29:24 +0100

    fixed docker mongo config

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -4,6 +4,6 @@ dotenv.config()
 // This should be your own database url
 module.exports = {
   database:
-    process.env.DATABASE||"mongodb://localhost:27017/alpa",
+    process.env.DATABASE||"mongodb://mongo:27018/launch",
   jwtsecret: process.env.SECRET||"secret-key-test",
 };
